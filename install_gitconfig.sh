139c25e1749e653cad4fd92a26524cf4655840f7
Guillaume Potier <cobalt2760@gmail.com>
2024-02-28 19:00:41 +0100

    gitconfig install

---
+++ install_gitconfig.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# this file is used to install the gitconfig file to the user's home directory
+# retrieve gitconfigfile here https://raw.githubusercontent.com/guillaumepotier/dotfiles/master/.gitconfig
+file=".gitconfig"
+url="https://raw.githubusercontent.com/guillaumepotier/dotfiles/master/.gitconfig"
+echo "Downloading $file from $url"
+
+# download the file
+curl -o $file $url
+
+# move the file to the user's home directory
+mv $file ~/.gitconfig
+
+echo "File $file has been installed in your home directory"
